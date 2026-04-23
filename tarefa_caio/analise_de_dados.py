5e2cc92ad78d6527e1742a7536f6c3a43c2084f2
Caio Lemos <116179963+kaiz0rd@users.noreply.github.com>
2026-04-23 10:57:08 -0300

    Update analise_de_dados.py

--- tarefa_caio/analise_de_dados.py
+++ tarefa_caio/analise_de_dados.py
@@ -4,4 +4,14 @@ import pandas as pd
 df = pd.read_csv("Student Mental Health.csv")
 
 # Para ver o resultado no terminal:
-print(df.head())
+# print(df.head())
+
+# Verificar os valores mínimos, máximos e a média de idade
+print(df['Age'].describe())
+
+print(df['Age'].unique())
+
+# Criando o filtro conforme o planejamento do projeto
+df_juventude = df[(df['Age'] >= 15) & (df['Age'] <= 29)]
+
+print(f"Total de registros na faixa de 15-29 anos: {len(df_juventude)}")
